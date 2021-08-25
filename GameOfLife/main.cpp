5fef9c3cb2e5fa1bdb7b451720ea0cd8a74be36b
Cristime <thecristime@outlook.com>
2021-08-26 04:15:27 +0800

    Formatted all C/C++ files and add config generator

--- GameOfLife/main.cpp
+++ GameOfLife/main.cpp
@@ -6,7 +6,7 @@
 */
 
 #include "header.h"
-#include "cmdline.h"	// 解析命令行参数
+#include "cmdline.h" // 解析命令行参数
 
 #include <fstream>
 #include <ctime>
@@ -16,7 +16,8 @@ bool mp[MAXL][MAXL], tmp[MAXL][MAXL];
 std::string fileName;
 cmdline::parser argParser;
 
-int main(int argc, char ** argv) {
+int main(int argc, char **argv)
+{
 	argParser.add<int>("length", 'l', "map length", false, 50);
 	argParser.add<int>("height", 'h', "map height", false, 50);
 	argParser.add<bool>("type", 't', "is rand map", false, true);
@@ -25,8 +26,10 @@ int main(int argc, char ** argv) {
 	argParser.parse_check(argc, argv);
 
 	Init();
-	while (true) {
-		if (!aliveNum) break;
+	while (true)
+	{
+		if (!aliveNum)
+			break;
 		Iteration();
 		PrintMap();
 	}
@@ -34,53 +37,65 @@ int main(int argc, char ** argv) {
 	return 0;
 }
 
-
-void Init() {
+void Init()
+{
 	n = argParser.get<int>("height");
 	m = argParser.get<int>("length");
-	
-	if (argParser.get<bool>("type") == false) {
+
+	if (argParser.get<bool>("type") == false)
+	{
 		unsigned x, y;
 
 		fileName = argParser.get<std::string>("config");
 		std::ifstream fin(fileName);
-		if (!fin.is_open()) {
+		if (!fin.is_open())
+		{
 			std::cerr << "Cannot open config file. Exiting..." << std::endl;
 			exit(1);
 		}
-		while (std::cin >> x >> y) {
+		while (std::cin >> x >> y)
+		{
 			mp[x][y] = 1;
 			++aliveNum;
 		}
 		fin.close();
-	} else {
+	}
+	else
+	{
 		srand(time(NULL));
 
 		for (int i = 1; i <= n; i++)
-			for (int j = 1; j <= m; j++) {
+			for (int j = 1; j <= m; j++)
+			{
 				mp[i][j] = rand() * rand() % 2;
 				aliveNum += mp[i][j] ? 1 : 0;
 			}
 	}
 }
 
-void CopyMap(bool src[MAXL][MAXL], bool dst[MAXL][MAXL]) {
+void CopyMap(bool src[MAXL][MAXL], bool dst[MAXL][MAXL])
+{
 	for (int i = 1; i <= n; i++)
 		for (int j = 1; j <= m; j++)
 			dst[i][j] = src[i][j];
 }
 
-void Iteration() {
+void Iteration()
+{
 	++iterNum;
 	CopyMap(mp, tmp);
 	for (int i = 1; i <= n; i++)
-		for (int j = 1; j <= m; j++) {
+		for (int j = 1; j <= m; j++)
+		{
 			unsigned nearAliveNum = 0;
 			for (int k = 0; k < 8; k++)
 				nearAliveNum += mp[i + dx[k]][j + dy[k]] ? 1 : 0;
-			if (nearAliveNum == 3) tmp[i][j] = 1;
-			else if (nearAliveNum == 2 && mp[i][j]) tmp[i][j] = 1;
-			else tmp[i][j] = 0;
+			if (nearAliveNum == 3)
+				tmp[i][j] = 1;
+			else if (nearAliveNum == 2 && mp[i][j])
+				tmp[i][j] = 1;
+			else
+				tmp[i][j] = 0;
 		}
 	CopyMap(tmp, mp);
 	aliveNum = 0;
@@ -89,10 +104,12 @@ void Iteration() {
 			++aliveNum;
 }
 
-void PrintMap() {
+void PrintMap()
+{
 	system("cls");
 	std::cout << "Iteration num: " << iterNum << std::endl;
-	for (int i = 1; i <= n; i++) {
+	for (int i = 1; i <= n; i++)
+	{
 		for (int j = 1; j <= m; j++)
 			std::cout.put(mp[i][j] ? 'O' : ' ');
 		std::cout.put('\n');
